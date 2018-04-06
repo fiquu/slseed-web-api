048626f982387b8a65c84f7f5916c062401e9857
Santiago G. Marín <santiago@finaldevstudio.com>
2018-04-06 10:48:09 -0300

    Added log retention config

--- configs/provider.js
+++ configs/provider.js
@@ -13,6 +13,7 @@ module.exports = {
   runtime: 'nodejs6.10',
   stage: '${opt:stage}',
 
+  logRetentionInDays: process.env.NODE_ENV === 'production' ? 14 : 7,
   memorySize: 512,
   timeout: 30,
 
