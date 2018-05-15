2a2fdee39c580c831a63157e51764e60f957cf60
Santiago G. Marín <santiago@finaldevstudio.com>
2018-05-15 10:09:46 -0400

    Cleaned up code

--- service/functions/test.js
+++ service/functions/test.js
@@ -6,10 +6,7 @@ const { NoContent } = require('../components/responses');
  * @returns {Object|Error} The response.
  */
 module.exports.handler = async event => {
-  console.log('Test function is working.');
-  console.dir(event, { colors: true });
-
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise(resolve => setTimeout(resolve(event), 1000));
 
   return new NoContent();
 };
