6b03ab51563cf6946d63712ec5980c19b81fcdd0
Santiago G. Marín <santiago@finaldevstudio.com>
2018-12-31 16:19:27 -0300

    Updated setup scripts

--- setup/index.js
+++ setup/index.js
@@ -1,9 +1,14 @@
 const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
 (async () => {
   const { setup } = await inquirer.prompt({
+    choices: fs
+      .readdirSync('./setup')
+      .filter(file => path.extname(file) === '.js' && path.basename(file, '.js') !== 'index')
+      .map(file => file.replace('.js', '')),
     message: 'Select setup',
-    choices: ['env', 'stack'],
     type: 'list',
     name: 'setup'
   });
