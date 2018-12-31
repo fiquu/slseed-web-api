aa30d9e6b7ae08f4382e047d0cd8393484d713a0
Santiago G. Marín <santiago@finaldevstudio.com>
2018-12-31 15:46:16 -0300

    Updated database component

--- service/configs/database.js
+++ service/configs/database.js
@@ -10,6 +10,7 @@ module.exports = {
     promiseLibrary: Promise,
     connectTimeoutMS: 30000, // Match default HTTP timeout
     socketTimeoutMS: 30000, // Match default HTTP timeout
+    useNewUrlParser: true,
     autoIndex: false, // You should use the db-indexes script to create the database indexes
     poolSize: 1 // We don't need more for each function
   }
