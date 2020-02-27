e114a21aa4952c9c8b219f30e4b3a543ee29be9e
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-02-27 17:49:31 -0300

    Fixed functions loader

--- configs/functions.js
+++ configs/functions.js
@@ -4,16 +4,14 @@
  * @module configs/functions
  */
 
-const { posix, join, resolve, sep } = require('path');
+const { join, resolve, sep, dirname } = require('path');
 const glob = require('glob');
 const slug = require('slug');
-const is = require('fi-is');
-
 const pattern = join(process.cwd(), 'service', 'functions', '**', 'config.js');
 const files = glob.sync(resolve(pattern));
 
 for (const file of files) {
-  const name = slug(file.replace(/^.+\/services\/functions\/(.+)\/config\.js$/, '$1-$2'), {
+  const name = slug(file.replace(/^.+\/service\/functions\/(.+)\/config\.js$/, '$1'), {
     lower: true,
     charmap: {
       ...slug.charmap,
@@ -21,17 +19,10 @@ for (const file of files) {
     }
   });
 
-  const basePath = file.replace(/^.+\/service\/functions\/.+\/config\.js$/, '');
   const path = file.replace(process.cwd(), '').replace(/^\/config\.js$/, '');
   const config = require(file);
 
-  for (const { http } of config.events) {
-    if (is.not.empty(http)) {
-      http.path = posix.normalize(posix.join('/', basePath, http.path));
-    }
-  }
-
-  config.handler = `${join(path, 'handler')}.handler`;
+  config.handler = `${join(dirname(path), 'handler')}.handler`;
   config.name = name;
 
   module.exports[name] = config;
