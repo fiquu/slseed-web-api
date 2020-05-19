ad3092834a45caaace628cbf7842bf51e24070fa
Santiago G. Marín <santiago.marin@final.cl>
2020-05-19 12:02:23 -0400

    Added minimum compression setting

--- configs/serverless/provider.js
+++ configs/serverless/provider.js
@@ -5,6 +5,9 @@ module.exports = {
   runtime: 'nodejs12.x',
   stage: '${opt:stage}',
   name: 'aws',
+  apiGateway: {
+    minimumCompressionSize: 128
+  },
 
   logRetentionInDays: process.env.NODE_ENV === 'production' ? 14 : 7,
   memorySize: 512,
