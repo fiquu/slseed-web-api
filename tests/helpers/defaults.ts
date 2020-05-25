5641328cb55c3283d4a4554721abf0bcd5dc1262
Santiago G. Marín <santiago.marin@final.cl>
2020-05-25 09:41:28 -0400

    Improved defaults

--- tests/helpers/defaults.ts
+++ tests/helpers/defaults.ts
@@ -1,29 +1,12 @@
 import chaiAsPromised from 'chai-as-promised';
 import dotenv from 'dotenv';
-import AWS from 'aws-sdk';
-import path from 'path';
 import chai from 'chai';
 
-import { region, profiles } from '../../configs/aws';
-
 dotenv.config({
-  path: path.resolve(process.cwd(), '.env.test')
+  path: '.env.test'
 });
 
-process.env.MONGOMS_SYSTEM_BINARY = '/usr/bin/mongod';
+process.env.MONGOMS_SYSTEM_BINARY = process.env.MONGOMS_SYSTEM_BINARY || '/usr/bin/mongod';
 process.env.SERVERLESS_TEST_ROOT = '../../service';
-
-const profile = profiles[String(process.env.NODE_ENV)];
-
-process.env.AWS_PROFILE = String(profile);
-
-const credentials: AWS.SharedIniFileCredentials = new AWS.SharedIniFileCredentials({
-  profile: profiles[String(profile)]
-});
-
-AWS.config.update({
-  credentials,
-  region
-});
 
 chai.use(chaiAsPromised);
