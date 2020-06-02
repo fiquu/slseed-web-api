2a0d6326ba36f1fd8ac94e470b0d1969da2a0d11
Santiago G. Marín <santiago.marin@final.cl>
2020-06-02 16:52:02 -0400

    Improved email templating

--- scripts/ses/templates/upload.ts
+++ scripts/ses/templates/upload.ts
@@ -2,14 +2,15 @@ import confirmPrompt from '@fiquu/slseed-web-utils/lib/confirm-prompt';
 import stageSelect from '@fiquu/slseed-web-utils/lib/stage-select';
 import { minify } from 'html-minifier';
 import htmlToText from 'html-to-text';
-import { readFileSync } from 'fs';
 import { prompt } from 'inquirer';
+import { basename } from 'path';
 import dotenv from 'dotenv';
 import juice from 'juice';
 import chalk from 'chalk';
 import AWS from 'aws-sdk';
 import glob from 'glob';
 import ora from 'ora';
+import pug from 'pug';
 
 const spinner = ora();
 
@@ -26,7 +27,7 @@ console.log(`${chalk.cyan.bold('SES Email Template Upload Script')}\n`);
 
   spinner.start('Listing templates...');
 
-  const files = glob.sync('./email/**/*.html');
+  const files = glob.sync('./email/templates/**/*.pug');
 
   spinner.stop();
 
@@ -34,30 +35,33 @@ console.log(`${chalk.cyan.bold('SES Email Template Upload Script')}\n`);
     {
       name: 'file',
       type: 'list',
-      message: 'Select HTML template:',
+      message: 'Select email template:',
       default: true,
-      choices: files.map(file => file.replace(process.cwd(), ''))
+      choices: files.map(file => ({
+        name: `${basename(file, '.pug')} (${file.replace(process.cwd(), '')})`,
+        value: file.replace(process.cwd(), '')
+      }))
     }
   ]);
 
   spinner.start('Reading and parsing template...');
 
-  const template = readFileSync(file).toString();
-  let TemplateName: string;
-  let SubjectPart: string;
+  let template: string;
 
-  // Extract template values
-  template.split(/\r?\n/).forEach(line => {
-    const _line = line.trim();
+  try {
+    template = minify(pug.renderFile(file), {
+      collapseWhitespace: true,
+      removeComments: false, // Leave comments so we can parse the info
+      minifyCSS: true
+    });
+  } catch (err) {
+    spinner.fail(err.message);
 
-    if (/^Template\.TemplateName\s*=\s*.+$/.test(_line)) {
-      TemplateName = _line.replace(/^Template\.TemplateName\s*=\s*(.+)$/, '$1');
-    }
+    throw err;
+  }
 
-    if (/^Template\.SubjectPart\s*=\s*.+$/.test(_line)) {
-      SubjectPart = _line.replace(/^Template\.SubjectPart\s*=\s*(.+)$/, '$1');
-    }
-  });
+  const TemplateName = template.replace(/.*SES\.TemplateName\s*=\s*"([^"]+)".*/, '$1');
+  const SubjectPart = template.replace(/.*SES\.SubjectPart\s*=\s*"([^"]+)".*/, '$1');
 
   spinner.info(`${chalk.bold('TemplateName: ')}${TemplateName}`);
   spinner.info(`${chalk.bold('SubjectPart:  ')}${SubjectPart}`);
@@ -83,11 +87,9 @@ console.log(`${chalk.cyan.bold('SES Email Template Upload Script')}\n`);
         TextPart: htmlToText.fromString(template),
         TemplateName,
         SubjectPart,
-        HtmlPart: juice(minify(template, {
-          collapseWhitespace: true,
-          removeComments: true,
-          minifyCSS: true
-        }))
+        HtmlPart: minify(juice(template), {
+          removeComments: true // Now remove comments
+        })
       }
     };
 
@@ -116,5 +118,6 @@ console.log(`${chalk.cyan.bold('SES Email Template Upload Script')}\n`);
     spinner.succeed('Template created!');
   } catch (err) {
     spinner.fail(err.message);
+    process.exitCode = 1;
   }
 })();
