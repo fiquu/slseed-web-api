84f9fa84d9630bc08ec03105ffb6e2535e310aca
Santiago G. Marín <santiago.marin@final.cl>
2020-06-02 17:03:59 -0400

    Improved tests

---
+++ tests/email/welcome.spec.ts
@@ -0,0 +1,35 @@
+import SES from 'aws-sdk/clients/ses';
+import { expect } from 'chai';
+import faker from 'faker';
+
+const ses = new SES({
+  region: 'us-east-1'
+});
+
+describe('confirmation email', function () {
+  this.timeout(5000);
+
+  it('does not renders the template without proper values', async function () {
+    const task = ses.testRenderTemplate({
+      TemplateName: 'Welcome',
+      TemplateData: JSON.stringify({})
+    }).promise();
+
+    await expect(task).to.eventually.be.rejectedWith(Error);
+  });
+
+  it('properly renders the template with proper values', async function () {
+    const task = ses.testRenderTemplate({
+      TemplateName: 'Welcome',
+      TemplateData: JSON.stringify({
+        user: {
+          name: faker.name.firstName()
+        }
+      })
+    }).promise();
+
+    await expect(task).to.eventually.be.an('object').with.keys(
+      'ResponseMetadata', 'RenderedTemplate'
+    );
+  });
+});
