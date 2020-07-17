77eba4aab40e44b38d688f5b23a5bfcdf5874f20
Santiago G. Marín <santiago.marin@final.cl>
2020-07-17 10:22:56 -0400

    Added email test render

---
+++ tests/email/render.ts
@@ -0,0 +1,28 @@
+import { join, dirname, basename, extname, resolve } from 'path';
+import { fromString } from 'html-to-text';
+import { minify } from 'html-minifier';
+import juice from 'juice';
+import fs from 'fs-extra';
+import glob from 'glob';
+import pug from 'pug';
+
+const files = glob.sync('email/templates/**/*.pug');
+
+for (const file of files) {
+  const inFile = resolve(file);
+  const outFileHtml = `${basename(file, extname(file))}.html`;
+  const outFileText = `${basename(file, extname(file))}.txt`;
+  const outDir = join(__dirname, 'rendered', dirname(file.replace('email/', '')));
+  const html = pug.renderFile(inFile);
+
+  fs.ensureDirSync(outDir);
+  fs.ensureFileSync(join(outDir, outFileHtml));
+  fs.ensureFileSync(join(outDir, outFileText));
+
+  fs.writeFileSync(join(outDir, outFileHtml), minify(juice(html), {
+    removeComments: true,
+    minifyCSS: true
+  }));
+
+  fs.writeFileSync(join(outDir, outFileText), fromString(html));
+}
